0132573069e1c8e6aa5f8f3549521fc483f7a575
Kourosh Mobl <moblkourosh@gmail.com>
2025-09-24 15:58:32 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://kourosh.github.io",
+  site: 'https://kourosh.github.io',
 });
