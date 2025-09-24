8cbf6e3cd5f9335a268705653a739fe115f66d35
Kourosh Mobl <moblkourosh@gmail.com>
2025-09-24 16:09:13 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'http://kourosh.github.io',
+  site: 'https://kourosh.github.io',
 });
